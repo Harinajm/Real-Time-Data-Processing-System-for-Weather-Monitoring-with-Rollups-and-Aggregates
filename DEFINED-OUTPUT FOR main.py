6cdd752c6f21bffcc9e011736d9423e37930deea
Harinajm <126600584+Harinajm@users.noreply.github.com>
2024-10-21 16:52:39 +0530

    Create DEFINED-OUTPUT FOR main.py

---
+++ DEFINED-OUTPUT FOR main.py
@@ -0,0 +1,161 @@
+"C:\Users\HP\PycharmProjects\Rule Engine\.venv\Scripts\python.exe" "C:\Users\HP\PycharmProjects\Rule Engine\main.py" 
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning: datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+  timestamp = datetime.utcfromtimestamp(data['dt']).strftime('%Y-%m-%d %H:%M:%S')
+Daily Weather Summary for Delhi:
+Avg Temp: 32.05°C
+Max Temp: 32.05°C
+Min Temp: 32.05°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Mumbai:
+Avg Temp: 31.99°C
+Max Temp: 31.99°C
+Min Temp: 31.99°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Chennai:
+Avg Temp: 31.22°C
+Max Temp: 31.22°C
+Min Temp: 31.22°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Bangalore:
+Avg Temp: 27.20°C
+Max Temp: 27.20°C
+Min Temp: 27.20°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Kolkata:
+Avg Temp: 29.97°C
+Max Temp: 29.97°C
+Min Temp: 29.97°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Hyderabad:
+Avg Temp: 31.23°C
+Max Temp: 31.23°C
+Min Temp: 31.23°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Delhi:
+Avg Temp: 32.05°C
+Max Temp: 32.05°C
+Min Temp: 32.05°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Mumbai:
+Avg Temp: 31.99°C
+Max Temp: 31.99°C
+Min Temp: 31.99°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Chennai:
+Avg Temp: 31.27°C
+Max Temp: 31.32°C
+Min Temp: 31.22°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Bangalore:
+Avg Temp: 27.20°C
+Max Temp: 27.20°C
+Min Temp: 27.20°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Kolkata:
+Avg Temp: 29.97°C
+Max Temp: 29.97°C
+Min Temp: 29.97°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Hyderabad:
+Avg Temp: 31.23°C
+Max Temp: 31.23°C
+Min Temp: 31.23°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Delhi:
+Avg Temp: 32.05°C
+Max Temp: 32.05°C
+Min Temp: 32.05°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Mumbai:
+Avg Temp: 31.99°C
+Max Temp: 31.99°C
+Min Temp: 31.99°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Chennai:
+Avg Temp: 31.24°C
+Max Temp: 31.32°C
+Min Temp: 31.19°C
+Dominant Condition: Clouds
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Bangalore:
+Avg Temp: 27.32°C
+Max Temp: 27.57°C
+Min Temp: 27.20°C
+Dominant Condition: Rain
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Kolkata:
+Avg Temp: 29.97°C
+Max Temp: 29.97°C
+Min Temp: 29.97°C
+Dominant Condition: Haze
+C:\Users\HP\PycharmProjects\Rule Engine\main.py:63: DeprecationWarning:
+
+datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC).
+
+Daily Weather Summary for Hyderabad:
+Avg Temp: 31.23°C
+Max Temp: 31.23°C
+Min Temp: 31.23°C
+Dominant Condition: Rain
