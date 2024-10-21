7183d07ccb6772c73fc4bf1a1500d675aaec6b23
Harinajm <126600584+Harinajm@users.noreply.github.com>
2024-10-21 16:47:28 +0530

    Create OUTPUT app.py

---
+++ OUTPUT app.py
@@ -0,0 +1,9 @@
+"C:\Users\HP\PycharmProjects\Rule Engine\.venv\Scripts\python.exe" "C:\Users\HP\PycharmProjects\Rule Engine\app.py" 
+ * Serving Flask app 'app'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 268-523-583
